cd41ef5370a25107c38b8941e2fd5485f6b0e53f
MrBreakNFix <73456238+Anonymous17532@users.noreply.github.com>
2022-10-02 17:37:15 -0700

    Add files via upload

---
+++ src/main/java/net/fabricmc/noob/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.fabricmc.noob.mixin;
+
+import net.fabricmc.noob.NoobHack;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		NoobHack.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
